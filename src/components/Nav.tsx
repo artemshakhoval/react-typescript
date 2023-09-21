c5ee68c5fb7c445e3dae982c6df3fbd3f68a5bf0
shah <artemshakhoval@gmail.com>
2023-09-21 14:06:11 +0300

    import routers and create different pages to work with

---
+++ src/components/Nav.tsx
@@ -0,0 +1,11 @@
+import { Navbar, Container } from 'react-bootstrap'
+
+export function Nav() {
+    return (
+        <Navbar className='bg-white shadow-sm mb-3'>
+            <Container>
+                Nav
+            </Container>
+        </Navbar> 
+    )
+}
